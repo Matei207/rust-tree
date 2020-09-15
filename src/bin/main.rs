e2265e34217fa7eb5a089e93d025df94edc7220c
Matei207 <madavid@expediagroup.com>
2020-09-15 20:55:10 +0100

    Change file walk, files are now ignored
    Signed-off-by: Matei207 <madavid@expediagroup.com>

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,39 +1,11 @@
-use std::{fs, env};
 use std::io;
-use std::path::Path;
 
 fn main() -> io::Result<()> {
-    let current_dir = env::current_dir()?;
-    walk(&current_dir)?;
-    
+    rust_tree::run()?;
 
     Ok(())
 }
 
-fn walk(dir: &Path) -> io::Result<()> {
-    if dir.is_dir() {
-        match fs::read_dir(dir) {
-            Err(err) => println!("error {}", err),
-            Ok(paths) => for path in paths {
-                let entry = path.unwrap();
-                match entry.file_name().to_str() {
-                    Some(name) => println!("-- {}", name),
-                    _ => {}
-                }
-                
-                let path = entry.path();
-                if path.is_dir() {
-                    if !entry.path().starts_with(".") {
-                       walk(&path)?;
-                    }
-                }
-
-            }
-        }
-    }
-  
-    Ok(())
-}
 /*
     * '?' is shorthand for an entire match statement, if it's an error
     it returns from the function, if it is an ok it can unwraps
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,57 @@
+use std::{env, fs, io, path::Path};
 
+/*
+    * Day 3: make sure we can get the count of files
+    make sure we can travel our dir
+*/
+
+pub fn run() -> io::Result<()> {
+    let current_dir = env::current_dir()?;
+    println!("{}", current_dir.file_name().unwrap().to_str().unwrap());
+    walk(&current_dir, &mut String::from(""))?;
+
+    Ok(())
+}
+fn walk(dir: &Path, space: &mut String) -> io::Result<()> {
+    if dir.is_dir() {
+        let entries: Vec<_> = fs::read_dir(dir)?
+        .filter_map(|e| {
+            let entry = e.unwrap();
+            match entry.file_name().to_str() {
+                Some(name) => {
+                    if name.starts_with(".") || name == "target" {
+                        return None
+                    }
+                    Some(entry)
+                }
+
+                None => None
+            }
+        })
+        .collect();
+    
+        for (index, entry) in entries.iter().enumerate() {
+            let file_name = entry.file_name();
+            if index == entries.len() - 1 {
+                println!("{}└── {}", space, file_name.to_str().unwrap());
+            } else {
+                println!("{}├── {}", space, file_name.to_str().unwrap());
+            }
+
+            if entry.file_type().unwrap().is_dir() {
+                space.push_str("    ");
+                walk(&entry.path(), space)?;
+            }
+        }
+    }
+
+    Ok(())
+}
 
 #[cfg(test)]
 mod tests {
+    #[test]
+    fn print_current_directory() {}
     #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
@@ -9,6 +59,6 @@ mod tests {
 }
 
 /* Rules for imports:
-    * for function, import parent module
-    * for import structs do    
+ * for function, import parent module
+ * for import structs do
 */
